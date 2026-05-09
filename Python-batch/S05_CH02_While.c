6eca2cf8754d814c641de439b6aa2b97aad9d7f3
vish0201 <rabariv832@gmail.com>
2026-05-09 16:41:44 +0530

    loops in c

---
+++ Python-batch/S05_CH02_While.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+
+    int i = 6;
+    while (i<=5)
+    {
+        printf("Hello %d\n", i);
+        i++;
+    }
+
+    printf("\n");
+
+    return 0;
+}
---
+++ Python-batch/S05_CH03_Do_While.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+
+    int i = 6;
+
+    do
+    {
+        printf("%d  Hello\n", i);
+        i++;
+
+    } while (i <= 5);
+
+    printf("\n");
+
+    return 0;
+}
