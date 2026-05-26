c147087eecc694b41117d157c9dfb9a8ebefeb8a
vish0201 <rabariv832@gmail.com>
2026-05-26 13:20:32 +0530

    control statement

---
+++ Khushboo-python/M4_S03_CH01_Break.py
@@ -0,0 +1,13 @@
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+
+
+count = 1
+while True:
+   print(count)
+   if count == 4:
+      break
+   count += 1
---
+++ Khushboo-python/M4_S03_CH02_Continue.py
@@ -0,0 +1,13 @@
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
+
+
+
+count = 0
+while count < 5:
+ count += 1
+ if count == 3:
+    continue
+ print(count)
