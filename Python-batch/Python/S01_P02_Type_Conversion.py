7beaba792f09373364a9fa15431489236e4361d1
vish0201 <rabariv832@gmail.com>
2026-06-01 16:34:22 +0530

    variable and data types

--- Python-batch/Python/S01_P02_Type_Conversion.py
+++ Python-batch/Python/S01_P02_Type_Conversion.py
@@ -1,15 +1,27 @@
-price   = 1239.33
-price2   = 1350
+# Convert float to int
+num = 10.75  # float
+num_int =int(num)
+print(num_int)
 
 
 
+#convert int to float
+print("------------------")
+num2 = 190
+num_float = float(num2)
+print(num_float)
+
+#convert number to string
+print("------------------")
+num3 = 59
+print(type(num3))
+num_str = str(num3)
+print(num_str)
 
 
-bill_Amount= int(price)
-bill_Amount2= float(price2)
+#convert int to float 
 
-
-print(bill_Amount)
-print(bill_Amount2)
-
-
+num4 = 1456
+num_floati = float(num4)
+print(num_floati)
+ 
