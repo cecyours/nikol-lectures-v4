71958e00f17a7aa4a7de1164f94fb1bfdb29a7c9
vish0201 <rabariv832@gmail.com>
2026-06-02 19:38:16 +0530

    control-statemetn-> break  , continue

---
+++ Clang-2126/S10_P03_combined.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    for (int i = 0; i < 10; i++)
+    {
+
+        if (i == 5)
+            continue;
+
+        if (i == 7)
+            break;
+
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
