5c1513d87305684f195ea3c29866e63014aa3e2c
vish0201 <rabariv832@gmail.com>
2026-06-02 12:21:01 +0530

    nested loop

---
+++ Khushboo-python/M4_S04_CH01_Nested_loop.py
@@ -0,0 +1,21 @@
+# for i  in range(1 , 4):
+#     for j in range(1 , 4):
+#         print( "i = " , i ," | J = " , j)
+
+
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print(i * j, end=" ")
+#     print()
+
+
+
+rows = 10
+for i in range(1, rows + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+
+
