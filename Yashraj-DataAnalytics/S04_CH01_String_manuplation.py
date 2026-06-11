535e3d6d7fc899da76991900fd3b1728020fe84b
vish0201 <rabariv832@gmail.com>
2026-06-11 17:16:13 +0530

    string manuplate and formaring

---
+++ Yashraj-DataAnalytics/S04_CH01_String_manuplation.py
@@ -0,0 +1,36 @@
+str1 =  'hello'
+print(str1)
+str2 =  "world"
+print(str2)
+str3 = """ 
+my 
+name is 
+yashraj
+"""
+print(str3)
+
+first_name = "yashraj"
+lastname = "chauhan"
+full_name  = first_name + " " + lastname
+
+print("Full Name:", full_name)
+
+
+age = 20
+print("I am " + str(age) + " years old.")
+
+
+text = "PythonProgramming"
+
+print(text[0:6:3])
+
+print(text[-1])
+
+print(text[-11:])
+
+
+# Reverse the string
+print(text[::-1])
+
+
+
