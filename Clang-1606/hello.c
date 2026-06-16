4652dd8280a546d332057b70b6bc1fff7046ba5c
vish0201 <rabariv832@gmail.com>
2026-06-16 18:36:21 +0530

    hello world

---
+++ Clang-1606/hello.c
@@ -0,0 +1,22 @@
+// Documentation Section
+// This program will print hello world
+
+// preprocessor Section
+#include <stdio.h>
+#define PI 3.14
+
+// Global Variable
+int a = 100;
+
+// Main section
+int main()
+{
+    printf("Hello world");
+}
+
+// user define functionssection
+int greet()
+{
+    int b = 10;
+    printf("%d", a);
+}
