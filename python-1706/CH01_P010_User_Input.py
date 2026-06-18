768894f85f4fb956aa57d22264872c34604105dc
vish0201 <rabariv832@gmail.com>
2026-06-18 11:16:20 +0530

    data types

---
+++ python-1706/CH01_P010_User_Input.py
@@ -0,0 +1,4 @@
+#Take input from the user
+value =input("Enter the value:   ")
+#Print the message with entered value
+print("you entered:  " , value)
---
+++ python-1706/CH02_P01_Data_Types.py
@@ -0,0 +1,22 @@
+# Data Types in Python
+
+# 1.Integer(int)
+age = 22  # 1 , 18494 , 234543324
+while1 = 45
+
+# 2.float 
+pi= 3.14  
+
+# 3.String (str)
+name = "VISHAL" # "1" , "ADFVSA"  , "COMPUTER" , "True"
+
+# 4.boolean (bool)
+isPass = True # True , False
+
+
+print("Integer" , age)
+print("Float" , pi)
+print("String" , name)
+print("Boolean" , isPass)
+
+
