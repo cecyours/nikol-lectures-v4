2db0d38e4d0e2d3e68337fec544e3a2ef0ec883b
vish0201 <rabariv832@gmail.com>
2026-06-19 11:18:21 +0530

    Type Conversion

---
+++ python-1706/CH02_P02_Type_Conversion.py
@@ -0,0 +1,24 @@
+#  Convert float to integer
+price = 99.99
+print("Type before convert" , price)
+whole_price = int(price)
+print("Type after convert" , whole_price)
+
+# Convert integer to float
+age = 91
+print("Age Before Convert" , age)
+age_float = float(age)
+print("Age after convert" , age_float)
+
+# Convert number to string
+num = 20
+print("Num before convert" , num)
+print("Type of num is" , type(num))
+
+
+num_str = str(num)
+print("Num after convert" , num_str)
+print("Type of num_str is" , type(num_str))
+
+
+print(type("1"))
