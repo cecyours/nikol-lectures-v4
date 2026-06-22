cfd134ab115d4c6df3f32999741c83232fbb135c
vish0201 <rabariv832@gmail.com>
2026-06-22 17:05:43 +0530

    mutable vs immutable

---
+++ python-1706/CH02_P05_Immutable.py
@@ -0,0 +1,9 @@
+x = 10
+
+print("Value of x before update"  , id(x))
+
+
+x+=5 # x = x + 5
+
+
+print("Value of x after update"  , id(x))
---
+++ python-1706/CH02_P06_mutable.py
@@ -0,0 +1,8 @@
+num = [1 , 2 , 3]
+
+print("address of x before update"  , id(num))
+
+
+num.append(4)
+
+print("address of x after update"  , id(num))
