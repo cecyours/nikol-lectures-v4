0a3fb3d89797610033261ff752541c5af62b93ab
vish0201 <rabariv832@gmail.com>
2026-06-22 14:47:24 +0530

    variable and memory allocation

---
+++ python-1706/CH02_P03_Variable.py
@@ -0,0 +1,10 @@
+x=10
+y  = "Python"
+print(x)
+print(y)
+
+# Dynamic Typing
+var = 10 # var is an integer
+print(var, type(var))
+var = "Hello" # now var is a string
+print(var, type(var))
---
+++ python-1706/CH02_P04_memory_allocation.py
@@ -0,0 +1,9 @@
+a = 100
+b = a
+
+
+print("memory Adress of a" , id(a))
+print("memory Adress of b" , id(b))
+
+
+print(a, b)
