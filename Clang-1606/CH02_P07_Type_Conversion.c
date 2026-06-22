2489d2e3a3263e76827c138f9cb1b7a59ee46523
vish0201 <rabariv832@gmail.com>
2026-06-22 17:08:03 +0530

    implicite and explicite

---
+++ Clang-1606/CH02_P07_Type_Conversion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10  ;
+    float b = 9.7;
+
+    float result;
+
+    result = a + b;
+    printf("Result = %.2f\n", result);
+}
---
+++ Clang-1606/CH02_P08_Type_Conversion_explicite.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float a = 9.7;
+    int b;
+
+    b = (int)a;
+
+    printf("Float a = %f\n", a);
+    printf("Integer b = %d\n", b);
+}
