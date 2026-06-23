ba71d6d2603f95494fe5584929c1229d9cbfb844
vish0201 <rabariv832@gmail.com>
2026-06-23 19:04:00 +0530

    oprators

---
+++ Clang-1606/CH02_P011_Arithmetic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Evaluated By Preprocessor
+#define PI 3.14159
+int main()
+{
+
+    // const
+    const int age = 25;
+    const float pi = 3.14159;
+
+    printf("Integer a = %d\n", age);
+    printf("Value of PI = %f\n", pi);
+}
---
+++ Clang-1606/CH02_P012_Relational.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 11;
+
+
+
+    printf("Value of a is %d\n"  , a);
+    printf("Value of b is %d\n"  , b);
+
+
+    printf("a == b | %d\n", a == b);
+    printf("a != b | %d\n", a != b);
+    printf("a > b | %d\n", a > b);
+    printf("a < b | %d\n", a < b);
+    printf("a >= b | %d\n", a >= b);
+    printf("a <= b | %d\n", a <= b);
+
+
+
+
+}
