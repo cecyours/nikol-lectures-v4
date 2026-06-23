e90b07071a170ba44aa2931f9455470cbc7ccf1a
vish0201 <rabariv832@gmail.com>
2026-06-23 09:13:03 +0530

    variable and constants

---
+++ Clang-1606/CH02_P09_Variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int age = 25;
+    float salary = 45000.50;
+    char grade = 'A';
+
+    printf("Integer a = %d\n", age);
+    printf("Float b = %f\n", salary);
+    printf("Character c = %c\n", grade);
+}
