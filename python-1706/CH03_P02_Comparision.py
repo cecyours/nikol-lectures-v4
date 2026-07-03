7e7496804776b48826ef551a12c42047d9a48c59
vish0201 <rabariv832@gmail.com>
2026-07-03 11:50:12 +0530

    oprators

---
+++ python-1706/CH03_P02_Comparision.py
@@ -0,0 +1,13 @@
+a = 48  
+b = 24
+
+
+print( "a == b", a == b)  # false
+print("a != b" , a != b) # True
+print("a > b" , a > b) # True 
+print("a < b" , a < b) # False 
+print("a >= b" , a >= b) # True 
+print("a <= b" , a <= b) # False 
+
+
+
---
+++ python-1706/CH03_P04_Assignment.py
@@ -0,0 +1,27 @@
+x = 10
+print("Original value of x ="  , x)
+
+x+= 20
+print("After +=  x is" , x)
+
+x-= 20
+print("After -=  x is" , x)
+
+x*=10
+print("After *=  x is" , x)
+
+x/=5
+print("After /=  x is" , x)
+
+x//=2
+print("After //=  x is" , int(x))
+
+x%=3
+print("After %=  x is" , x)
+
+x**=3
+print("After **=  x is" , x)
+
+
+
+
