524b998fd3283740f3b5314cbd2d5cffa76be6ee
vish0201 <rabariv832@gmail.com>
2026-07-03 18:16:49 +0530

    function pass value and ref

---
+++ Clang-1606/CH04_P04_Call_by_value.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void increment(int num)
+{
+    num = num + 1;
+    printf("Inside function: %d\n", num);
+}
+
+int main()
+{
+
+    int n = 5;
+    increment(n);
+
+
+    printf("Value : %d\n", n);
+}
