6f397b257d296c362c338ff3981c14bb9ef7b033
vish0201 <rabariv832@gmail.com>
2026-07-08 11:07:23 +0530

    truth table

---
+++ python-1706/CH03_P09_Truth_Table.py
@@ -0,0 +1,4 @@
+for a in [True , False]:
+    for b in [True , False]:
+        print(f"a={a}  , b = {b} , a and b = {a and b} ")
+        
---
+++ python-1706/CH03_P10_Bool_condition.py
@@ -0,0 +1,7 @@
+age = 18
+income = 40000
+
+if age >= 18 and income >30000:
+    print("Eligible for loan")
+else:
+    print("Not eligible")
