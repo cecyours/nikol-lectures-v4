35326dc569238548e2c62b9ea8250a5d18ffc97a
vish0201 <rabariv832@gmail.com>
2026-07-10 18:38:20 +0530

    Linear search

---
+++ Clang-1606/CH05_P05_Linear.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int arr[5] = {10, 20, 30, 40, 50};
+    int i, key = 30, found = 0;
+    for (i = 0; i < 5; i++)
+    {
+        if (arr[i] == key)
+        {
+            found = 1;
+            break; // exit loop once found
+        }
+    }
+    if (found)
+        printf("%d found at index %d\n", key, i);
+    else
+        printf("%d not found\n", key);
+    return 0;
+}
