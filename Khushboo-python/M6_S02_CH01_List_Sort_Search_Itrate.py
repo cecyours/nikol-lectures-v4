4c0bc33aff2187a864194233c5f33b8c9ae955e9
vish0201 <rabariv832@gmail.com>
2026-07-21 17:06:32 +0530

    searchig and sorting

---
+++ Khushboo-python/M6_S02_CH01_List_Sort_Search_Itrate.py
@@ -0,0 +1,23 @@
+# numbers = [70 ,80 , 10 , 20 , 30]
+# # asc
+# # numbers.sort()
+
+# # for decending 
+# numbers.sort(reverse=True)
+
+# print("Sorted list:", numbers)
+
+numbers = [8, 3, 6, 1, 9]
+sorted_list = sorted(numbers)
+print("Original list:", numbers)
+print("Sorted list:", sorted_list)
+print("Original list:", numbers)
+
+
+# Searching
+
+numbers = [10, 20, 30, 40]
+if 99 in numbers:
+    print("Element found in the list")
+else:
+    print("Element not found")
