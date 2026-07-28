16c2ee3ed4cf93e605d3ed28825c7b01679b6879
vish0201 <rabariv832@gmail.com>
2026-07-28 10:57:50 +0530

    return parameters and example

---
+++ python-1706/CH05_P08_F_Return_Multiple.py
@@ -0,0 +1,11 @@
+def calculate(a , b):
+    sum = a + b
+    product = a * b
+    return sum , product
+
+
+
+x , y  = calculate(1 , 2)
+
+print("Sum is" , x)
+print("Product is" , y)
---
+++ python-1706/CH05_P09_F_Parameter_ex.py
@@ -0,0 +1,16 @@
+# def power(base, exponent=2):
+#     result = base ** exponent
+#     # result = 5 ** 2 = 25
+#     # result = 5 ** 3 = 125
+#     print("Result =", result)
+
+
+
+# power(5)
+# power(5, 3)
+
+def employee(name , salary ,department):
+    print("Name:", name)
+    print("Salary:", salary)
+    print("Department:", department)
+employee("Amit", department="IT", salary=50000)
