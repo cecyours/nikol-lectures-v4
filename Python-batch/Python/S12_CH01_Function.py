49342e96f4f1ecf1c5f1e80ac155b139b4618898
vish0201 <rabariv832@gmail.com>
2026-07-28 17:00:37 +0530

    function with parameter

---
+++ Python-batch/Python/S12_CH01_Function.py
@@ -0,0 +1,14 @@
+# Defining a Function
+def welcome_user(name):
+    print(f"Hello {name} , I am from Function" )
+
+
+# Calling a Function
+welcome_user("Yashraj")
+welcome_user("Manan")
+welcome_user("Vish")
+
+welcome_user("khush")
+
+welcome_user("hola")
+
---
+++ Python-batch/Python/S12_CH02_Function_parameter.py
@@ -0,0 +1,24 @@
+# function with multiple parameter
+def add(a , b , c, d = 16):
+    print(a + b + c +d)
+
+add(1 , 2 ,2 )
+add(2 , 2 , 3 , 12) 
+add(5984, 45453 , 9 , 18)
+
+
+
+
+# # Defining a Function
+# def welcome_user(name):
+#     print(f"Hello {name} , I am from Function" )
+
+# # Calling a Function
+# welcome_user("Yashraj")
+# welcome_user("Manan")
+# welcome_user("Vish")
+
+# welcome_user("khush")
+
+# welcome_user("hola")
+
