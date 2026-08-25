a62d548826b9c2dc470802ec899584b8cbdbb3f3
vish0201 <rabariv832@gmail.com>
2026-08-25 18:11:06 +0530

    list

---
+++ Yashraj-DataAnalytics/S07_CH02_List_Sorting.py
@@ -0,0 +1,19 @@
+numbers = [30, 50, 20, 10, 60, 40]
+
+print(numbers)
+
+# numbers.sort(reverse=True)
+
+
+
+new_num = sorted(numbers)
+
+
+print(numbers)
+
+print(new_num)
+
+
+
+
+
---
+++ Yashraj-DataAnalytics/S07_CH03_List_Searching.py
@@ -0,0 +1,6 @@
+numbers = [30, 50, 20, 10, 60, 40]
+
+if 40 in numbers:
+    print("Found")
+else:
+    print("Not Found")
